21ffaa7c837ad5ccedee46b88050cafe3b5c0c5a
juliogarciag <julio@withbliss.ai>
2024-01-19 16:35:08 -0500

    Add eye icon to show/hide password (+ add tailwind)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FormEvent, useRef } from "react";
-import "./App.css";
+import { FormEvent, useRef, useState } from "react";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 function App() {
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -23,16 +23,46 @@ function App() {
     }
   };
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="card">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          ref={passwordRef}
-          className="password"
-          autoFocus
-        />
-        <button type="submit" className="button">
+    <div className="flex flex-row p-4 w-[256px]">
+      <form onSubmit={handleSubmit} className="w-full">
+        <div className="w-full relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            ref={passwordRef}
+            className="p-1 pr-8 mb-2 w-full"
+            autoFocus
+          />
+          <button
+            className="w-4 h-4 absolute top-[5px] right-2.5"
+            type="button"
+            onClick={() => {
+              setShowPassword(!showPassword);
+
+              const input = passwordRef.current;
+              if (input) {
+                input.focus();
+
+                requestIdleCallback(() => {
+                  input.selectionStart = input.value.length;
+                  input.selectionEnd = input.value.length;
+                });
+              }
+            }}
+          >
+            {showPassword ? (
+              <EyeSlashIcon className="w-full h-full" />
+            ) : (
+              <EyeIcon className="w-full h-full" />
+            )}
+          </button>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-emerald-600 text-white px-1 py-2 font-bold"
+        >
           Enter
         </button>
       </form>

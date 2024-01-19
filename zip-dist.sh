1bd6a98ce501b8eb2efb2806c89a6b9a0d1fa6d9
juliogarciag <julio@withbliss.ai>
2024-01-19 16:57:15 -0500

    Update extension file and update zip-dist.sh

--- zip-dist.sh
+++ zip-dist.sh
@@ -1,1 +1,1 @@
- cp -r dist interbank-type-extension && zip interbank-type-extension.zip interbank-type-extension && rm -rf interbank-type-extension
+ cp -r dist interbank-type-extension && zip -r interbank-type-extension.zip interbank-type-extension && rm -rf interbank-type-extension
